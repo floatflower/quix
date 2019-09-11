01d4923ab78f31946dc2a243aec977e19cd8d1cd
floatflower <zxc110888asd@gmail.com>
2019-09-11 15:01:35 +0800

    Update

--- bin/lib/dash-to-camel-case.js
+++ bin/lib/dash-to-camel-case.js
@@ -1,6 +1,6 @@
 module.exports = (data) => {
     let converted = data.replace(/(-[a-z])/g, function (g) { return g[1].toUpperCase(); });
-    converted = (converted[0].toUpperCase()) + converted.substr(0, converted.length - 1);
+    converted = (converted[0].toUpperCase()) + converted.substr(1, converted.length - 1);
 
     return converted;
 };
